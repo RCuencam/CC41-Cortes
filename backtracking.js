94ac83df2c517de932b0a53aa057e8137358bca9
mirandolahere <miranda.95.cm@gmail.com>
2019-09-24 16:47:37 -0500

    backtracking

---
+++ backtracking.js
@@ -0,0 +1,79 @@
+
+var la, an;
+var canvas, body; 
+var ctx;
+var padre =[{x:800, y: 400}];
+//
+var rectangulos = [  {x:100,y:50},{x:80,y:40},{x:200,y:50},
+{x:150,y:50},{x:100,y:20},{x:50,y:30},{x:60,y:10},{x:70,y:20}];
+var rectOrden=[];
+var newPoints=[];
+var obj ={}
+function send()
+{
+    rectOrden = rectangulos.sort(function (a, b){
+        return (b.x  - a.x)
+    })
+    for(i = 0 ; i<1;i++)
+    {
+                 obj ={x : 0 ,
+                        y: 0,
+                        largo :rectOrden[0].x,
+                        ancho : rectOrden[0].y};
+                        newPoints.push(obj);
+
+    }
+    console.log(newPoints)
+    canvas = document.createElement('canvas');
+    
+    canvas.id = "CursorLayer";
+    canvas.width = padre[0].x;
+    canvas.height = padre[0].y;
+    canvas.style.zIndex = 8;
+    canvas.style.position = "absolute";
+    canvas.style.border = "1px solid";
+
+
+    body = document.getElementById("area");
+    body.appendChild(canvas);
+
+    cursorLayer = document.getElementById("CursorLayer");
+    ctx = cursorLayer.getContext("2d");
+
+    ctx.fillStyle = "rgb(100,100,00)";
+    
+        for(a = 0;a<newPoints.length ; a++)
+        {  
+            ctx.fillRect(newPoints[a].x, newPoints[a].y ,newPoints[a].largo ,newPoints[a].ancho); // x , y, largo,ancho
+
+        }
+}
+
+function x(data) {
+    la = document.getElementById("largo").value;
+    an= document.getElementById("ancho").value;
+   console.log(la)
+   console.log(an)
+
+    canvas = document.createElement('canvas');
+   
+        canvas.id = "CursorLayer";
+        canvas.width = la;
+        canvas.height = an;
+        canvas.style.zIndex = 8;
+        canvas.style.position = "absolute";
+        canvas.style.border = "1px solid";
+
+
+        var body = document.getElementById("area");
+        body.appendChild(canvas);
+
+        cursorLayer = document.getElementById("CursorLayer");
+
+        console.log(cursorLayer);
+}
+
+/*ctx.fillStyle = "rgb(100,100,00)";
+ctx.fillRect(0,0,100,50); // x , y, largo,ancho
+
+*/
