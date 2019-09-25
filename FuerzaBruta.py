1ee1e5e1870f2f32cc3c1c4b39c89b139cc0a8eb
Rodrigo <u201710092@upc.edu.pe>
2019-09-25 17:56:05 -0500

    Ordenar Areas

--- FuerzaBruta.py
+++ FuerzaBruta.py
@@ -1,1 +1,20 @@
-print("hola")
+def CalcularArea(A,H):
+    n=len(A)
+    resultado=[]
+
+    for i in range(n):
+        producto=A[i]*H[i]
+        resultado.append(producto)
+    return resultado
+
+def Ordenar(resultado):
+    resultado.sort(reverse=True)
+    return resultado
+    
+
+def Cortes()
+
+A=[4,5,6,7]
+H=[1,2,3,4]
+
+print(Ordenar(CalcularArea(A,H)))
