6516de4c674bea736722a6ee82686a9bcb6d0976
Rodrigo <u201710092@upc.edu.pe>
2019-09-25 21:40:32 -0500

    Ordenar[area,ancho,alto]

--- FuerzaBruta.py
+++ FuerzaBruta.py
@@ -1,15 +1,25 @@
+A=[4,5,6,7]
+H=[1,2,3,4]
 def MostrarAreas(A,H):
     n=len(A)
     resultado=[]
 
     for i in range(n):
         producto=A[i]*H[i]
-        resultado.append(producto)
+
+        resultado.append((producto,A[i],H[i]))
     return resultado
+	
 
 def Ordenar(resultado):
-    resultado.sort(reverse=True)
-    return resultado
+	lengthOfArray = len(resultado) - 1
+	for i in range(lengthOfArray):
+		for j in range(lengthOfArray - i):
+			if resultado[j][0] < resultado[j + 1][0]:
+				resultado[j], resultado[j + 1] = resultado[j + 1], resultado[j]
+	return resultado
+
+
     
 def AreaTotal(resultado):
     n=len(resultado)
@@ -23,10 +33,9 @@ def Desperdicio():
     resultado=100-((AreaTotal(MostrarAreas(A,H))/planchaArea)*100)
     return resultado
 
-A=[4,5,6,7]
-H=[1,2,3,4]
-planchaArea=200
+
+print(MostrarAreas(A,H))
+print(Ordenar(MostrarAreas(A,H)))
 
 
-print(AreaTotal(MostrarAreas(A,H)))
-print(Desperdicio(),"%")
+
